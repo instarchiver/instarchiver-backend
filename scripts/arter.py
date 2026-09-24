bbc1f212c087ac35f238dcc8e7dea7d3fe04ad35
Arter Tendean <arter.tendean.07@gmail.com>
2026-09-25 00:14:38 +0700

    Implement classification for Instagram URLs and add tests for new functionality

--- scripts/arter.py
+++ scripts/arter.py
@@ -1,6 +1,16 @@
-from instagram.models import Story  # noqa: INP001
+from instagram.utils import categorize_instagram_url  # noqa: INP001
+
+SAMPLE_URLS = [
+    "https://www.instagram.com/stories/natgeo/3864373243738778145/",
+    "https://www.instagram.com/p/C1a2b3c4d5e/",
+    "https://www.instagram.com/reel/C1a2b3c4d5e/",
+    "https://www.instagram.com/stories/highlights/17954206126712345/",
+    "https://www.instagram.com/reels/audio/1234567890123456/",
+    "https://www.instagram.com/natgeo/",
+    "https://chatgpt.com/",
+]
 
 
 def run():
-    story = Story.objects.get(story_id="3864373243738778145")
-    story.generate_embedding()
+    for url in SAMPLE_URLS:
+        print(categorize_instagram_url(url), ": ", url)  # noqa: T201
